dc88eeda792198fe98843b836ef74605d86ad222
Nick Sanguinetti <DJInsomniaPro@gmail.com>
2020-03-04 21:50:21 -0600

    Cleanup

--- oot_mq_dbg/403 - En_Nutsball/En_Nutsball_asm.c
+++ oot_mq_dbg/403 - En_Nutsball/En_Nutsball_asm.c
@@ -21,54 +21,42 @@ typedef struct {
 	/* uint8_t debug_ex[16];*/
 	z64_actorfunc_t* state;
 	uint8_t object_index;
-	uint8_t inst0151; /* Does not appear to be used. */
+	uint8_t inst0151;
 	int16_t timer;
 	z64_collider_cylinder_main_t collider;
 } entity_t; /* 01A0 */
 
 /*** function prototypes ***/
-void dest(entity_t* en, z64_global_t* gl);
-void set_draw(entity_t* en, z64_global_t* gl);
-void handle_collider(entity_t* en, z64_global_t* gl);
-void init(entity_t* en, z64_global_t* gl);
-void draw(entity_t* en, z64_global_t* gl);
-void update(entity_t* en, z64_global_t* gl);
+void data_80ABBB0C(entity_t* en, z64_global_t* gl);
+#define OK_80ABBB0C 1
+void data_80ABBB34(entity_t* en, z64_global_t* gl);
+#define OK_80ABBB34 1
+void data_80ABBBA8(entity_t* en, z64_global_t* gl);
+#define OK_80ABBBA8 1
+void data_80ABBA50(entity_t* en, z64_global_t* gl);
+#define OK_80ABBA50 1
+void data_80ABBE90(entity_t* en, z64_global_t* gl);
+#define OK_80ABBE90 1
+void data_80ABBD74(entity_t* en, z64_global_t* gl);
+#define OK_80ABBD74 1
 
 
 /*** variables ***/
-const z64_collider_cylinder_init_t collider_init = {
-    . body = {
-        .unk_0x14 = 0x0A,
-        .collider_flags = 0x11,
-        .collide_flags = 0x09,
-        .mask_a = 0x39,
-        .mask_b = 0x20,
-        .type = COL_TYPE_CYLINDER,
-        .unk_0x06_ = 0x0000,
-        .body_flags = 0x00,
-        .unk_0x09_ = 0x000000,
-        .toucher_mask = 0xFFCFFFFF,
-        .bumper_effect = BUMP_FX_KNOCKBACK,
-        .toucher_damage = 0x08,
-        .unk_0x12_ = 0x0000,
-        .bumper_mask = 0xFFCFFFFF,
-        .unk_0x18_ = 0x00000000,
-        .toucher_flags = 0x11,
-        .bumper_flags = 0x01,
-        .body_flags_2 = 0x01,
-        .unk_0x1F = 0x00
-    },
-    .radius = 13,
-    .height = 13,
-    .y_shift = 0,
-    .position = {
-        .x = 0,
-        .y = 0,
-        .z = 0
-    }
+const uint32_t data_80ABBFC0[] =
+{
+	0x0A110939,
+	0x20010000,
+	0x00000000,
+	0xFFCFFFFF,
+	0x00080000,
+	0xFFCFFFFF,
+	0x00000000,
+	0x11010100,
+	0x000D000D,
+	0x00000000,
+	0x00000000
 };
-
-const uint16_t obj_index[] =
+const uint16_t data_80ABBFEC[] =
 {
 	OBJ_DEKUNUTS,
 	OBJ_HINTNUTS,
@@ -78,7 +66,7 @@ const uint16_t obj_index[] =
 	0
 };
 
-const uint32_t dl_index[] =
+const uint32_t data_80ABBFF8[] =
 {
 	DL_DEKUNUTS,
 	DL_HINTNUTS,
@@ -88,28 +76,85 @@ const uint32_t dl_index[] =
 	0
 };
 
-/*** functions ***/
-/* Main Destructor Function */
-void dest(entity_t* en, z64_global_t* gl)
+const uint32_t data_80ABC010[] =
 {
+	0x2E2E2F7A,
+	0x5F656E5F,
+	0x6E757473,
+	0x62616C6C,
+	0x2E630000
+};
+
+const uint32_t data_80ABC024[] =
+{
+	0x2E2E2F7A,
+	0x5F656E5F,
+	0x6E757473,
+	0x62616C6C,
+	0x2E630000
+};
+
+const uint32_t data_80ABC038[] =
+{
+	0x2E2E2F7A,
+	0x5F656E5F,
+	0x6E757473,
+	0x62616C6C,
+	0x2E630000
+};
+
+const uint32_t data_80ABC04C[] =
+{
+	0x38C90FDA
+};
+
+
+/*** functions ***/
+#if OK_80ABBB0C
+void data_80ABBB0C(entity_t* en, z64_global_t* gl) /* 0 internal, 1 external, 10 lines */
+{
+	asm(
+		".set at        \n"
+		".set reorder   \n"
+		".Ldata_80ABBB0C: \n"
+	);
 	z_collider_cylinder_free(gl, &en->collider);
 }
+#else
+#include "asm/data_80ABBB0C.c"
+#endif
 
-void set_draw(entity_t* en, z64_global_t* gl) /* 0 internal, 1 external, 30 lines */
+#if OK_80ABBB34
+void data_80ABBB34(entity_t* en, z64_global_t* gl) /* 0 internal, 1 external, 30 lines */
 {
+	asm(
+		".set at        \n"
+		".set reorder   \n"
+		".Ldata_80ABBB34: \n"
+	);
+
 	if (z_scene_object_is_loaded(&gl->obj_ctxt, en->object_index))
 	{
-		(en->actor).draw_proc = (void*)draw;
+		(en->actor).draw_proc = (void*)data_80ABBE90;
 		(en->actor).rot_2.y = 0;
 		en->timer = 30;
-		en->state = (z64_actorfunc_t*)handle_collider;
+		en->state = (z64_actorfunc_t*)data_80ABBBA8;
 		(en->actor).alloc_index = en->object_index;
 		(en->actor).xz_speed = 10.0f;
 	}
 }
+#else
+#include "asm/data_80ABBB34.c"
+#endif
 
-void handle_collider(entity_t* en, z64_global_t* gl) /* 0 internal, 4 external, 124 lines */
+#if OK_80ABBBA8
+void data_80ABBBA8(entity_t* en, z64_global_t* gl) /* 0 internal, 4 external, 124 lines */
 {
+  asm(
+      ".set at        \n"
+      ".set reorder   \n"
+      ".Ldata_80ABBBA8: \n"
+  );
   z64_player_t* Link = zh_get_player(gl);
   vec3f_t fx_pos;
   vec3s_t _dir;
@@ -149,29 +194,48 @@ LAB_destroy:
   fx_pos.z = en->actor.pos_2.z;
 
   z_effect_spawn_hahen_n(gl, &fx_pos, 6.0f, 0, 7, 3, 15, -1, 10, 0);
-  z_sfx_play_position(gl, &en->actor.pos_2, 0x14, NA_SE_EN_OCTAROCK_ROCK);
+  z_sfx_play_position(gl, &en->actor.pos_2, 0x14, 0x38C0);
   z_actor_kill(&en->actor);
 }
+#else
+#include "asm/data_80ABBBA8.c"
+#endif
 
-void init(entity_t* en, z64_global_t* gl) /* 0 internal, 5 external, 49 lines */
+#if OK_80ABBA50
+void data_80ABBA50(entity_t* en, z64_global_t* gl) /* 0 internal, 5 external, 49 lines */
 {
+	asm(
+		".set at        \n"
+		".set reorder   \n"
+		".Ldata_80ABBA50: \n"
+	);
 	/* Initialize the shadow rendering process and load the collider structure into memory. */
 	z_actor_shadow_init(&(en->actor).rot_2, 400.0f, &Z_SHADOW_CIRCLE, 13.0f);
 	z_collider_cylinder_alloc(gl, &en->collider);
-	z_collider_cylinder_init(gl, &en->collider, &en->actor, &collider_init);
+	z_collider_cylinder_init(gl, &en->collider, &en->actor, (z64_collider_cylinder_init_t*)data_80ABBFC0);
 
 	/* Grab the correct deku scrub object from the index list. */
-	en->object_index = z_scene_object_get_index(&gl->obj_ctxt, obj_index[(en->actor).variable]);
+	en->object_index = z_scene_object_get_index(&gl->obj_ctxt, data_80ABBFEC[(en->actor).variable]);
 
 	/* If the object isn't loaded, kill the actor process; otherwise, continue. */
 	if (en->object_index < 0)
 		z_actor_kill(&en->actor);
 	else
-		en->state = (z64_actorfunc_t*)set_draw;
+		en->state = (z64_actorfunc_t*)data_80ABBB34;
 }
+#else
+#include "asm/data_80ABBA50.c"
+#endif
 
-void draw(entity_t* en, z64_global_t* gl) /* 0 internal, 6 external, 68 lines */
+#if OK_80ABBE90
+void data_80ABBE90(entity_t* en, z64_global_t* gl) /* 0 internal, 6 external, 68 lines */
 {
+	asm(
+		".set at        \n"
+		".set reorder   \n"
+		".Ldata_80ABBE90: \n"
+	);
+
 	z64_gfx_t* gfx = (gl->common).gfx_ctxt;
 	z64_disp_buf_t* opa = &ZQDL(gl, poly_opa);
 	Gfx gfx_debug[3];
@@ -183,39 +247,51 @@ void draw(entity_t* en, z64_global_t* gl) /* 0 internal, 6 external, 68 lines */
 	z_matrix_roll(((en->actor).rot_init.z * 0.000095873795f), 1);
 	gSPMatrix(opa->p++, z_matrix_alloc(gfx, "../z_en_nutsball.c"/*, __LINE__*/), G_MTX_LOAD);
 	/* Grab the display list pointer form the index, based on the variable.*/
-	gSPDisplayList(opa->p++, dl_index[(en->actor).variable]);
+	gSPDisplayList(opa->p++, data_80ABBFF8[(en->actor).variable]);
 	z_debug_graph_write(gfx_debug, gfx, "../z_en_nutsball.c", __LINE__);
 }
+#else
+#include "asm/data_80ABBE90.c"
+#endif
 
-void update(entity_t* en, z64_global_t* gl) /* 0 internal, 6 external, 73 lines */
+#if OK_80ABBD74
+void data_80ABBD74(entity_t* en, z64_global_t* gl) /* 0 internal, 6 external, 73 lines */
 {
+	asm(
+		".set at        \n"
+		".set reorder   \n"
+		".Ldata_80ABBD74: \n"
+	);
 	z64_actorfunc_t* state_func = en->state;
 	z64_player_t* Link = zh_get_player(gl);
 
-	if ((Link->state_flags_1 & 0x300000C0) != 0 && state_func != (z64_actorfunc_t*)set_draw)
+	if ((Link->state_flags_1 & 0x300000C0) != 0 && state_func != (z64_actorfunc_t*)data_80ABBB34)
 			return;
 
 	if (state_func)
 		state_func(en, gl);
 
 	z_actor_move_dir_vel(&en->actor);
-	z_actor_find_bounds(gl, &en->actor, 10.0f, collider_init.radius, collider_init.height, 5);
+	z_actor_find_bounds(gl, &en->actor, 10.0f, /* collider_init.radius*/0x0D, /* collider_init.height*/0x0D, 5);
 	z_collider_cylinder_update(&en->actor, &en->collider);
 	(en->actor).flags |= 0x01000000;
 	z_collider_set_at(gl, &gl->hit_ctxt, &en->collider);
 	z_collider_set_ac(gl, &gl->hit_ctxt, &en->collider);
 	z_collider_set_ot(gl, &gl->hit_ctxt, &en->collider);
 }
+#else
+#include "asm/data_80ABBD74.c"
+#endif
 
 const z64_actor_init_t init_vars = {
-	.number = 0xDEAD, .padding = 0xBEEF, /* These are used by zzromtool or zzrtl to detect initialization variables. */
+	.number = 0xDEAD, .padding = 0xBEEF, /* <-- magic values, do not change */
 	.type = 0x06,
 	.room = 0x00,
 	.flags = 0x00000010,
 	.object = OBJ_ID,
 	.instance_size = sizeof(entity_t),
-	.init = init,
-	.dest = dest,
-	.main = update,
+	.init = data_80ABBA50,
+	.dest = data_80ABBB0C,
+	.main = data_80ABBD74,
 	.draw = NULL
 };
